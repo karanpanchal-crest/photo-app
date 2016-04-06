e29771d4898f2336fdf14fe2a3fee7267430d9af
alpha-blog <karan.panchal@crestdatasys.com>
2016-04-06 13:58:32 +0530

    Added payment functionality in the back end

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,13 +18,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {:host => "http://10.0.1.74:5000"}
   
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
   :address => 'smtp.gmail.com',
   :port => '587',
   :domain => "10.0.1.74:5000",
   :authentication => :plain,
   enable_starttls_auto: true,
-  :username => ENV["gmail_username"],
+  :user_name => ENV["gmail_username"],
   :password => ENV["gmail_password"]
   }
 
